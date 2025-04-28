8624878dfee4844dc7177f871fa8420473939c0c
mahmoud-star680 <113873810+mahmoud-elsay@users.noreply.github.com>
2025-04-29 01:35:34 +0300

    Add login screen and routing updates

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -3,21 +3,22 @@ import 'package:twazoon/core/routing/routes.dart';
 import 'package:twazoon/features/onboarding/onboarding_screen.dart';
 
 class AppRouter {
-
   Route generateRoute(RouteSettings settings) {
     final arguments = settings.arguments;
     switch (settings.name) {
       case Routes.onBoardingScreen:
-        return MaterialPageRoute(
-          builder: (_) => const OnBoardingScreen(),
-        );
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
+
       default:
         return MaterialPageRoute(
-          builder: (_) => Scaffold(
-            body: Center(
-              child: Text('No route defined for ${settings.name}'),
-            ),
-          ),
+          builder:
+              (_) => Scaffold(
+                body: Center(
+                  child: Text('No route defined for ${settings.name}'),
+                ),
+              ),
         );
     }
   }
